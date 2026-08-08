a2939083747be54ad03197926c2c40f3003e5d7c
Aman Panwar <179289809+amanpanwar1@users.noreply.github.com>
2026-08-08 20:33:43 +0530

    Time: 0 ms (100%), Space: 46.7 MB (7.69%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.java
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -3,6 +3,7 @@ class Solution {
         int ans[] = new int[2*n];
         ans[0] = nums[0];
         int x = 1; 
+        
         int y = n;
         for(int i = 1 ; i < 2*n ; i++) { 
             if (i % 2 == 0) {
